bd062fa5cf5ffb80000cede17371199d9145d7ff
fmueller <felix.mueller.berlin@googlemail.com>
2014-01-26 13:30:54 +0100

    expanded goal features significantly

--- soccer-national-prediction/src/main/groovy/de/cupofjava/machinelearning/soccer/worldcup/feature/GoalAverages.groovy
+++ soccer-national-prediction/src/main/groovy/de/cupofjava/machinelearning/soccer/worldcup/feature/GoalAverages.groovy
@@ -11,20 +11,61 @@ final class GoalAverages implements Feature {
 
   @Override
   int getSize() {
-    8
+    32
   }
 
   @Override
   double[] compute(LocalDate matchDate, String homeTeam, String awayTeam) {
     double[] goalAverages = new double[getSize()]
-    goalAverages[0] = goalAverage(Matches.allHomeMatchesBefore(matchDate, homeTeam).grep{ it.isHomeWin() }, homeTeam)
-    goalAverages[1] = goalAverage(Matches.allAwayMatchesBefore(matchDate, awayTeam).grep{ it.isAwayWin() }, awayTeam)
-    goalAverages[2] = goalAverage(Matches.allHomeMatchesBefore(matchDate, homeTeam).grep{ it.isAwayWin() }, homeTeam)
-    goalAverages[3] = goalAverage(Matches.allAwayMatchesBefore(matchDate, awayTeam).grep{ it.isHomeWin() }, awayTeam)
-    goalAverages[4] = goalAverage(Matches.lastHomeMatchesBefore(matchDate, homeTeam).grep{ it.isHomeWin() }, homeTeam)
-    goalAverages[5] = goalAverage(Matches.lastAwayMatchesBefore(matchDate, awayTeam).grep{ it.isAwayWin() }, awayTeam)
-    goalAverages[6] = goalAverage(Matches.lastHomeMatchesBefore(matchDate, homeTeam).grep{ it.isAwayWin() }, homeTeam)
-    goalAverages[7] = goalAverage(Matches.lastAwayMatchesBefore(matchDate, awayTeam).grep{ it.isHomeWin() }, awayTeam)
+
+    def allMatchesOfHomeTeam = Matches.allMatchesBefore(matchDate, homeTeam)
+    goalAverages[0] = goalAverage(allMatchesOfHomeTeam, homeTeam)
+    goalAverages[1] = goalAverage(allMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalAverages[2] = goalAverage(allMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalAverages[3] = goalAverage(allMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def allMatchesOfAwayTeam = Matches.allMatchesBefore(matchDate, awayTeam)
+    goalAverages[4] = goalAverage(allMatchesOfAwayTeam, awayTeam)
+    goalAverages[5] = goalAverage(allMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalAverages[6] = goalAverage(allMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalAverages[7] = goalAverage(allMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
+    def lastMatchesOfHomeTeam = Matches.lastMatchesBefore(matchDate, homeTeam)
+    goalAverages[8] = goalAverage(lastMatchesOfHomeTeam, homeTeam)
+    goalAverages[9] = goalAverage(lastMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalAverages[10] = goalAverage(lastMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalAverages[11] = goalAverage(lastMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def lastMatchesOfAwayTeam = Matches.lastMatchesBefore(matchDate, awayTeam)
+    goalAverages[12] = goalAverage(lastMatchesOfAwayTeam, awayTeam)
+    goalAverages[13] = goalAverage(lastMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalAverages[14] = goalAverage(lastMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalAverages[15] = goalAverage(lastMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
+    def allHomeMatchesOfHomeTeam = Matches.allHomeMatchesBefore(matchDate, homeTeam)
+    goalAverages[16] = goalAverage(allHomeMatchesOfHomeTeam, homeTeam)
+    goalAverages[17] = goalAverage(allHomeMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalAverages[18] = goalAverage(allHomeMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalAverages[19] = goalAverage(allHomeMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def allAwayMatchesOfAwayTeam = Matches.allAwayMatchesBefore(matchDate, awayTeam)
+    goalAverages[20] = goalAverage(allAwayMatchesOfAwayTeam, awayTeam)
+    goalAverages[21] = goalAverage(allAwayMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalAverages[22] = goalAverage(allAwayMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalAverages[23] = goalAverage(allAwayMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
+    def lastHomeMatchesOfHomeTeam = Matches.lastHomeMatchesBefore(matchDate, homeTeam)
+    goalAverages[24] = goalAverage(lastHomeMatchesOfHomeTeam, homeTeam)
+    goalAverages[25] = goalAverage(lastHomeMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalAverages[26] = goalAverage(lastHomeMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalAverages[27] = goalAverage(lastHomeMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def lastAwayMatchesOfAwayTeam = Matches.lastAwayMatchesBefore(matchDate, awayTeam)
+    goalAverages[28] = goalAverage(lastAwayMatchesOfAwayTeam, awayTeam)
+    goalAverages[29] = goalAverage(lastAwayMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalAverages[30] = goalAverage(lastAwayMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalAverages[31] = goalAverage(lastAwayMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
     goalAverages
   }
 
--- soccer-national-prediction/src/main/groovy/de/cupofjava/machinelearning/soccer/worldcup/feature/GoalDifferences.groovy
+++ soccer-national-prediction/src/main/groovy/de/cupofjava/machinelearning/soccer/worldcup/feature/GoalDifferences.groovy
@@ -11,20 +11,61 @@ final class GoalDifferences implements Feature {
 
   @Override
   int getSize() {
-    8
+    32
   }
 
   @Override
   double[] compute(LocalDate matchDate, String homeTeam, String awayTeam) {
     double[] goalDifferences = new double[getSize()]
-    goalDifferences[0] = computeGoalDiff(Matches.allHomeMatchesBefore(matchDate, homeTeam), homeTeam)
-    goalDifferences[1] = computeGoalDiff(Matches.allMatchesBefore(matchDate, homeTeam), homeTeam)
-    goalDifferences[2] = computeGoalDiff(Matches.allAwayMatchesBefore(matchDate, awayTeam), awayTeam)
-    goalDifferences[3] = computeGoalDiff(Matches.allMatchesBefore(matchDate, awayTeam), awayTeam)
-    goalDifferences[4] = computeGoalDiff(Matches.lastHomeMatchesBefore(matchDate, homeTeam), homeTeam)
-    goalDifferences[5] = computeGoalDiff(Matches.lastMatchesBefore(matchDate, homeTeam), homeTeam)
-    goalDifferences[6] = computeGoalDiff(Matches.lastAwayMatchesBefore(matchDate, awayTeam), awayTeam)
-    goalDifferences[7] = computeGoalDiff(Matches.lastMatchesBefore(matchDate, awayTeam), awayTeam)
+
+    def allMatchesOfHomeTeam = Matches.allMatchesBefore(matchDate, homeTeam)
+    goalDifferences[0] = computeGoalDiff(allMatchesOfHomeTeam, homeTeam)
+    goalDifferences[1] = computeGoalDiff(allMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalDifferences[2] = computeGoalDiff(allMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalDifferences[3] = computeGoalDiff(allMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def allMatchesOfAwayTeam = Matches.allMatchesBefore(matchDate, awayTeam)
+    goalDifferences[4] = computeGoalDiff(allMatchesOfAwayTeam, awayTeam)
+    goalDifferences[5] = computeGoalDiff(allMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalDifferences[6] = computeGoalDiff(allMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalDifferences[7] = computeGoalDiff(allMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
+    def lastMatchesOfHomeTeam = Matches.lastMatchesBefore(matchDate, homeTeam)
+    goalDifferences[8] = computeGoalDiff(lastMatchesOfHomeTeam, homeTeam)
+    goalDifferences[9] = computeGoalDiff(lastMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalDifferences[10] = computeGoalDiff(lastMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalDifferences[11] = computeGoalDiff(lastMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def lastMatchesOfAwayTeam = Matches.lastMatchesBefore(matchDate, awayTeam)
+    goalDifferences[12] = computeGoalDiff(lastMatchesOfAwayTeam, awayTeam)
+    goalDifferences[13] = computeGoalDiff(lastMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalDifferences[14] = computeGoalDiff(lastMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalDifferences[15] = computeGoalDiff(lastMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
+    def allHomeMatchesOfHomeTeam = Matches.allHomeMatchesBefore(matchDate, homeTeam)
+    goalDifferences[16] = computeGoalDiff(allHomeMatchesOfHomeTeam, homeTeam)
+    goalDifferences[17] = computeGoalDiff(allHomeMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalDifferences[18] = computeGoalDiff(allHomeMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalDifferences[19] = computeGoalDiff(allHomeMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def allAwayMatchesOfAwayTeam = Matches.allAwayMatchesBefore(matchDate, awayTeam)
+    goalDifferences[20] = computeGoalDiff(allAwayMatchesOfAwayTeam, awayTeam)
+    goalDifferences[21] = computeGoalDiff(allAwayMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalDifferences[22] = computeGoalDiff(allAwayMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalDifferences[23] = computeGoalDiff(allAwayMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
+    def lastHomeMatchesOfHomeTeam = Matches.lastHomeMatchesBefore(matchDate, homeTeam)
+    goalDifferences[24] = computeGoalDiff(lastHomeMatchesOfHomeTeam, homeTeam)
+    goalDifferences[25] = computeGoalDiff(lastHomeMatchesOfHomeTeam.grep{ it.isHomeWin() }, homeTeam)
+    goalDifferences[26] = computeGoalDiff(lastHomeMatchesOfHomeTeam.grep{ it.isDraw() }, homeTeam)
+    goalDifferences[27] = computeGoalDiff(lastHomeMatchesOfHomeTeam.grep{ it.isAwayWin() }, homeTeam)
+
+    def lastAwayMatchesOfAwayTeam = Matches.lastAwayMatchesBefore(matchDate, awayTeam)
+    goalDifferences[28] = computeGoalDiff(lastAwayMatchesOfAwayTeam, awayTeam)
+    goalDifferences[29] = computeGoalDiff(lastAwayMatchesOfAwayTeam.grep{ it.isHomeWin() }, awayTeam)
+    goalDifferences[30] = computeGoalDiff(lastAwayMatchesOfAwayTeam.grep{ it.isDraw() }, awayTeam)
+    goalDifferences[31] = computeGoalDiff(lastAwayMatchesOfAwayTeam.grep{ it.isAwayWin() }, awayTeam)
+
     goalDifferences
   }
 
