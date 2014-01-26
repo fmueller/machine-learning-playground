1ed0942c20acc30a8fd015a45f345000f33a5dfb
fmueller <felix.mueller.berlin@googlemail.com>
2014-01-26 13:30:36 +0100

    added features for match statistics

---
+++ soccer-national-prediction/src/main/groovy/de/cupofjava/machinelearning/soccer/worldcup/feature/MatchStatistics.groovy
@@ -0,0 +1,81 @@
+package de.cupofjava.machinelearning.soccer.worldcup.feature
+
+import de.cupofjava.machinelearning.soccer.worldcup.Matches
+import org.joda.time.LocalDate
+
+/**
+ * This feature does not to work for national soccer matches. Will leave
+ * this code here, maybe it will be usefull in analyzing matches (later on).
+ *
+ * @author fmueller
+ */
+final class MatchStatistics implements Feature {
+
+  @Override
+  int getSize() {
+    32
+  }
+
+  @Override
+  double[] compute(LocalDate matchDate, String homeTeam, String awayTeam) {
+    double[] matchStatistics = new double[getSize()]
+
+    def allMatchesOfHomeTeam = Matches.allMatchesBefore(matchDate, homeTeam)
+    matchStatistics[0] = playedMatches(allMatchesOfHomeTeam)
+    matchStatistics[1] = countMatches(allMatchesOfHomeTeam, { it.isHomeWin() })
+    matchStatistics[2] = countMatches(allMatchesOfHomeTeam, { it.isDraw() })
+    matchStatistics[3] = countMatches(allMatchesOfHomeTeam, { it.isAwayWin() })
+
+    def allMatchesOfAwayTeam = Matches.allMatchesBefore(matchDate, awayTeam)
+    matchStatistics[4] = playedMatches(allMatchesOfAwayTeam)
+    matchStatistics[5] = countMatches(allMatchesOfAwayTeam, { it.isHomeWin() })
+    matchStatistics[6] = countMatches(allMatchesOfAwayTeam, { it.isDraw() })
+    matchStatistics[7] = countMatches(allMatchesOfAwayTeam, { it.isAwayWin() })
+
+    def lastMatchesOfHomeTeam = Matches.lastMatchesBefore(matchDate, homeTeam)
+    matchStatistics[8] = playedMatches(lastMatchesOfHomeTeam)
+    matchStatistics[9] = countMatches(lastMatchesOfHomeTeam, { it.isHomeWin() })
+    matchStatistics[10] = countMatches(lastMatchesOfHomeTeam, { it.isDraw() })
+    matchStatistics[11] = countMatches(lastMatchesOfHomeTeam, { it.isAwayWin() })
+
+    def lastMatchesOfAwayTeam = Matches.lastMatchesBefore(matchDate, awayTeam)
+    matchStatistics[12] = playedMatches(lastMatchesOfAwayTeam)
+    matchStatistics[13] = countMatches(lastMatchesOfAwayTeam, { it.isHomeWin() })
+    matchStatistics[14] = countMatches(lastMatchesOfAwayTeam, { it.isDraw() })
+    matchStatistics[15] = countMatches(lastMatchesOfAwayTeam, { it.isAwayWin() })
+
+    def allHomeMatchesOfHomeTeam = Matches.allHomeMatchesBefore(matchDate, homeTeam)
+    matchStatistics[16] = playedMatches(allHomeMatchesOfHomeTeam)
+    matchStatistics[17] = countMatches(allHomeMatchesOfHomeTeam, { it.isHomeWin() })
+    matchStatistics[18] = countMatches(allHomeMatchesOfHomeTeam, { it.isDraw() })
+    matchStatistics[19] = countMatches(allHomeMatchesOfHomeTeam, { it.isAwayWin() })
+
+    def allAwayMatchesOfAwayTeam = Matches.allAwayMatchesBefore(matchDate, awayTeam)
+    matchStatistics[20] = playedMatches(allAwayMatchesOfAwayTeam)
+    matchStatistics[21] = countMatches(allAwayMatchesOfAwayTeam, { it.isHomeWin() })
+    matchStatistics[22] = countMatches(allAwayMatchesOfAwayTeam, { it.isDraw() })
+    matchStatistics[23] = countMatches(allAwayMatchesOfAwayTeam, { it.isAwayWin() })
+
+    def lastHomeMatchesOfHomeTeam = Matches.lastHomeMatchesBefore(matchDate, homeTeam)
+    matchStatistics[24] = playedMatches(lastHomeMatchesOfHomeTeam)
+    matchStatistics[25] = countMatches(lastHomeMatchesOfHomeTeam, { it.isHomeWin() })
+    matchStatistics[26] = countMatches(lastHomeMatchesOfHomeTeam, { it.isDraw() })
+    matchStatistics[27] = countMatches(lastHomeMatchesOfHomeTeam, { it.isAwayWin() })
+
+    def lastAwayMatchesOfAwayTeam = Matches.lastAwayMatchesBefore(matchDate, awayTeam)
+    matchStatistics[28] = playedMatches(lastAwayMatchesOfAwayTeam)
+    matchStatistics[29] = countMatches(lastAwayMatchesOfAwayTeam, { it.isHomeWin() })
+    matchStatistics[30] = countMatches(lastAwayMatchesOfAwayTeam, { it.isDraw() })
+    matchStatistics[31] = countMatches(lastAwayMatchesOfAwayTeam, { it.isAwayWin() })
+
+    matchStatistics
+  }
+
+  private double playedMatches(matches) {
+    matches.size() / 100.0
+  }
+
+  private double countMatches(matches, typeOfMatch) {
+    matches.count(typeOfMatch) / Math.max(1.0, (double) matches.size())
+  }
+}
---
+++ soccer-national-prediction/src/main/groovy/de/cupofjava/machinelearning/soccer/worldcup/feature/MatchStatisticsDifferences.groovy
@@ -0,0 +1,58 @@
+package de.cupofjava.machinelearning.soccer.worldcup.feature
+
+import de.cupofjava.machinelearning.soccer.worldcup.Matches
+import org.joda.time.LocalDate
+
+/**
+ * @author fmueller
+ */
+final class MatchStatisticsDifferences implements Feature {
+
+  @Override
+  int getSize() {
+    16
+  }
+
+  @Override
+  double[] compute(LocalDate matchDate, String homeTeam, String awayTeam) {
+    double[] statisticsDiff = new double[getSize()]
+
+    def allMatchesOfHomeTeam = Matches.allMatchesBefore(matchDate, homeTeam)
+    def allMatchesOfAwayTeam = Matches.allMatchesBefore(matchDate, awayTeam)
+    statisticsDiff[0] = playedMatches(allMatchesOfHomeTeam) - playedMatches(allMatchesOfAwayTeam)
+    statisticsDiff[1] = countMatches(allMatchesOfHomeTeam, { it.isHomeWin() }) - countMatches(allMatchesOfAwayTeam, { it.isHomeWin() })
+    statisticsDiff[2] = countMatches(allMatchesOfHomeTeam, { it.isDraw() }) - countMatches(allMatchesOfAwayTeam, { it.isDraw() })
+    statisticsDiff[3] = countMatches(allMatchesOfHomeTeam, { it.isAwayWin() }) - countMatches(allMatchesOfAwayTeam, { it.isAwayWin() })
+
+    def lastMatchesOfHomeTeam = Matches.lastMatchesBefore(matchDate, homeTeam)
+    def lastMatchesOfAwayTeam = Matches.lastMatchesBefore(matchDate, awayTeam)
+    statisticsDiff[4] = playedMatches(lastMatchesOfHomeTeam) - playedMatches(lastMatchesOfAwayTeam)
+    statisticsDiff[5] = countMatches(lastMatchesOfHomeTeam, { it.isHomeWin() }) - countMatches(lastMatchesOfAwayTeam, { it.isHomeWin() })
+    statisticsDiff[6] = countMatches(lastMatchesOfHomeTeam, { it.isDraw() }) - countMatches(lastMatchesOfAwayTeam, { it.isDraw() })
+    statisticsDiff[7] = countMatches(lastMatchesOfHomeTeam, { it.isAwayWin() }) - countMatches(lastMatchesOfAwayTeam, { it.isAwayWin() })
+
+    def allHomeMatchesOfHomeTeam = Matches.allHomeMatchesBefore(matchDate, homeTeam)
+    def allAwayMatchesOfAwayTeam = Matches.allAwayMatchesBefore(matchDate, awayTeam)
+    statisticsDiff[8] = playedMatches(allHomeMatchesOfHomeTeam) - playedMatches(allAwayMatchesOfAwayTeam)
+    statisticsDiff[9] = countMatches(allHomeMatchesOfHomeTeam, { it.isHomeWin() }) - countMatches(allAwayMatchesOfAwayTeam, { it.isAwayWin() })
+    statisticsDiff[10] = countMatches(allHomeMatchesOfHomeTeam, { it.isDraw() }) - countMatches(allAwayMatchesOfAwayTeam, { it.isDraw() })
+    statisticsDiff[11] = countMatches(allHomeMatchesOfHomeTeam, { it.isAwayWin() }) - countMatches(allAwayMatchesOfAwayTeam, { it.isHomeWin() })
+
+    def lastHomeMatchesOfHomeTeam = Matches.lastHomeMatchesBefore(matchDate, homeTeam)
+    def lastAwayMatchesOfAwayTeam = Matches.lastAwayMatchesBefore(matchDate, awayTeam)
+    statisticsDiff[12] = playedMatches(lastHomeMatchesOfHomeTeam) - playedMatches(lastAwayMatchesOfAwayTeam)
+    statisticsDiff[13] = countMatches(lastHomeMatchesOfHomeTeam, { it.isHomeWin() }) - countMatches(lastAwayMatchesOfAwayTeam, { it.isAwayWin() })
+    statisticsDiff[14] = countMatches(lastHomeMatchesOfHomeTeam, { it.isDraw() }) - countMatches(lastAwayMatchesOfAwayTeam, { it.isDraw() })
+    statisticsDiff[15] = countMatches(lastHomeMatchesOfHomeTeam, { it.isAwayWin() }) - countMatches(lastAwayMatchesOfAwayTeam, { it.isHomeWin() })
+
+    statisticsDiff
+  }
+
+  private double playedMatches(matches) {
+    matches.size() / 100.0
+  }
+
+  private double countMatches(matches, typeOfMatch) {
+    matches.count(typeOfMatch) / Math.max(1.0, (double) matches.size())
+  }
+}
